b2c0e8a249e4245d4bcbdc070af7f84c4ca43aba
Yermek Tuzelkhan <252080@astanait.edu.kz>
2026-01-22 11:32:51 +0500

    Tuzelkhan Yermek - > added main

--- src/platform/Main.java
+++ src/platform/Main.java
@@ -1,13 +1,103 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+package platform;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import platform.entity.Course;
+import platform.entity.User;
+import platform.entity.Lesson;
+import platform.repository.CourseRepository;
+import platform.repository.LessonRepository;
+import platform.service.*;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner cin = new Scanner(System.in);
+        CourseService courseService = new CourseService();
+        LessonService lessonService = new LessonService();
+        ProgressService progressService = new ProgressService();
+
+        CourseRepository courseRepo = new CourseRepository();
+        LessonRepository lessonRepo = new LessonRepository();
+        while (true) {
+            System.out.println("""
+                    
+                    1. Create course
+                    2. Create lesson
+                    3. Enroll user to course
+                    4. Open lesson
+                    5. Mark lesson completed
+                    6. Show course progress
+                    7. Show all courses
+                    0. Exit
+                    """);
+            int choice = cin.nextInt();
+            cin.nextLine();
+            switch (choice) {
+                case 1 -> {
+                    System.out.print("Course title: ");
+                    //  cin.nextLine();
+                    String title = cin.nextLine();
+                    Course fg = courseRepo.save(title);
+                    System.out.println("Course created successfully, ID: " + fg.getId());
+                }
+                case 2 -> {
+                    System.out.print("Course ID: ");
+                    Long courseId = cin.nextLong();
+
+                    System.out.print("Lesson title: ");
+                    cin.nextLine();
+                    String lessonTitle = cin.nextLine();
+                    lessonRepo.save(courseId, lessonTitle);
+                    System.out.println("Lesson created");
+                }
+                case 3 -> {
+                    System.out.print("User ID: ");
+                    Long userId = cin.nextLong();
+                    System.out.print("Course ID: ");
+                    Long courseId = cin.nextLong();
+                    courseService.enroll(Math.toIntExact(userId), Math.toIntExact(courseId));
+                    System.out.println("User enrolled");
+                }
+                case 4 -> {
+                    System.out.print("User ID: ");
+                    Long userId = cin.nextLong();
+                    System.out.print("Lesson ID: ");
+                    Long lessonId = cin.nextLong();
+                    lessonService.openLesson(userId, lessonId);
+                    System.out.println("Lesson opened");
+                }
+                case 5 -> {
+                    System.out.print("User ID: ");
+                    Long userId = cin.nextLong();
+                    System.out.print("Lesson ID: ");
+                    Long lessonId = cin.nextLong();
+                    progressService.markCompleted(Math.toIntExact(userId), Math.toIntExact(lessonId));
+                    System.out.println("Lesson marked as completed");
+                }
+                case 6 -> {
+                    System.out.print("User ID: ");
+                    Long userId = cin.nextLong();
+
+                    System.out.print("Course ID: ");
+                    Long courseId = cin.nextLong();
+                    int gh = (int) progressService.getProgress(Math.toIntExact(userId), Math.toIntExact(courseId));
+
+                    System.out.println("Course progress: " + gh + "%");
+                }
+                case 7 -> {
+                    System.out.println("All courses:");
+                    for (Course c : courseService.listAllCourses()) {
+                        System.out.println(c.getId() + " | " + c.getTitle());
+                    }
+                }
+                case 0 -> {
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                    break;
+                }
+                default -> System.out.println("Invalid option");
+            }
+        }
     }
 }
