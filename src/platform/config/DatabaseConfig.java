1b6f4d0bc41c65ca49783044fb58fa0aa592b04e
mikameeeeq <meirambekkausar20071216@gmail.com>
2026-02-01 17:01:29 +0500

    Kaussar Meirambekkyzy - added singulation in databaseconfig

--- src/platform/config/DatabaseConfig.java
+++ src/platform/config/DatabaseConfig.java
@@ -1,36 +1,43 @@
 package platform.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-import java.io.FileInputStream;
-import java.io.IOException;
-public class DatabaseConfig {
-    private static final String URL =
-            "jdbc:postgresql://aws-1-ap-northeast-2.pooler.supabase.com:5432/postgres?sslmode=require";
-    private static final String USER = "postgres.eblswscbdczprhchtdpx";
-    private static final String PASSWORD ;
 
-    static {
+public class DatabaseConfig {
+    private static DatabaseConfig instance;
+    private final String url = "jdbc:postgresql://aws-1-ap-northeast-2.pooler.supabase.com:5432/postgres?sslmode=require";
+    private final String user = "postgres.eblswscbdczprhchtdpx";
+    private final String password;
+
+    private DatabaseConfig() {
         Properties properties = new Properties();
         try (FileInputStream fis = new FileInputStream("config.properties")) {
             properties.load(fis);
-            PASSWORD = properties.getProperty("DB_PASSWORD");
+            password = properties.getProperty("DB_PASSWORD");
         } catch (IOException e) {
             throw new RuntimeException("Cannot load database password", e);
         }
-
-    }
-    private DatabaseConfig() {
-
     }
 
-    public static Connection getConnection() throws SQLException {
+    public static DatabaseConfig getInstance() {
+        if (instance == null) {
+            synchronized (DatabaseConfig.class) {
+                if (instance == null) instance = new DatabaseConfig();
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url, user, password);
     }
 }
